5c31525673c6f1bb450e8c5899e415fcfdf3f107
Valdomiro Filho <valdomiro.filho@boavista.local>
2021-02-15 17:49:36 -0300

    primeiro commit

---
+++ src/main/java/br/com/boavista/marketplaceconfigserver/MarketplaceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.boavista.marketplaceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceConfigServerApplication.class, args);
+	}
+
+}
